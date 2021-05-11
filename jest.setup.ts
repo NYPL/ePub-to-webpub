84ac180d906ddec47daf69995cc4552bee5971be
kristojorg <kristo.jorgenson@gmail.com>
2021-05-11 16:21:23 -0700

    msw update

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,7 +2,7 @@ import { server } from './mocks/server';
 // Establish API mocking before all tests.
 beforeAll(() =>
   server.listen({
-    onUnhandledRequest: 'warn',
+    onUnhandledRequest: 'error',
   })
 );
 
