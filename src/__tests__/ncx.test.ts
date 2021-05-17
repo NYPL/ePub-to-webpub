7e999c8f496cbdf5c5e54c135a997b125cf5ac49
kristojorg <kristo.jorgenson@gmail.com>
2021-05-17 09:20:25 -0700

    failing test

--- src/__tests__/ncx.test.ts
+++ src/__tests__/ncx.test.ts
@@ -1,5 +1,7 @@
 import { navPointToLink } from '../construct-manifest';
 import { ncxString } from './ncxStub';
+import xpath from 'xpath';
+import { DOMParser } from 'xmldom';
 
 describe('NCX Deserializer', () => {
   const navPoint = `
@@ -82,5 +84,45 @@ describe('NCX Deserializer', () => {
         },
       ],
     });
+  });
+
+  const strToDoc = (xml: string) => new DOMParser().parseFromString(xml);
+
+  /**
+   * we want to first query the name of the top level navPoint,
+   * then use the navPoint/navPoint query to get the children which are
+   * navPoints, and query the name of the inner navPoint there.
+   */
+  it('xpath queries using correct context', () => {
+    const navPointDoc = strToDoc(navPoint);
+    const labelNode = xpath.select(
+      'navPoint/navLabel/text/text()',
+      navPointDoc,
+      true
+    ) as Node;
+    const label = labelNode.nodeValue;
+    // we can successfully get the top level label
+    expect(label).toBe('Moby Dick');
+
+    // then we would recurse through the children navPoints, but
+    // in this case we will just take a single one to show how it doesn't
+    // work.
+    const children = xpath.select('navPoint/navPoint', navPointDoc);
+    // we have to case because the xpath library types are not great.
+    // should be a node though it if finds anything, which in this case it does
+    // find the right node.
+    const child1 = children[0] as Node;
+    const child1NewDoc = strToDoc(child1.toString());
+    // try to select the label using child1 as the context, but it will alwaus
+    // return undefined.
+    const childLabelNode = xpath.select(
+      'navPoint/navLabel/text/text()',
+      child1, // child1NewDoc works here, but child1 doesn't
+      true
+    ) as Node;
+    // it will fail here because childLabelNode is undefined
+    const childLabel = childLabelNode.nodeValue;
+
+    expect(childLabel).toBe('Child 1');
   });
 });
