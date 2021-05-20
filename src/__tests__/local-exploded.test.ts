ddc52e939a2a9cd3caa1ca56246603863e29cac2
kristojorg <kristo.jorgenson@gmail.com>
2021-05-19 17:30:37 -0700

    reading order

--- src/__tests__/local-exploded.test.ts
+++ src/__tests__/local-exploded.test.ts
@@ -2,31 +2,51 @@ import path from 'path';
 import { localExploded } from '..';
 import explodedMobyManifest from './stubs/exploded-moby-manifest.json';
 
-describe('EPUB 2', () => {
-  it('contructs Moby Dick Manifest', async () => {
+describe('EPUB 2', async () => {
+  async function getManifest() {
     const containerXmlPath = path.resolve(
       __dirname,
       '../../samples/moby-epub-exploded/META-INF/container.xml'
     );
-    const manifest = await localExploded(containerXmlPath);
+    return await localExploded(containerXmlPath);
+  }
+
+  it('context', async () => {
+    const manifest = await getManifest();
     expect(manifest['@context']).toBe(explodedMobyManifest['@context']);
+  });
+
+  it('extracts correct metadata', async () => {
+    const manifest = await getManifest();
     // metadata
     expect(explodedMobyManifest.metadata).toEqual(
       expect.objectContaining(manifest.metadata)
     );
     // make sure it has the contributors
     expect(manifest.metadata.author).toBe(explodedMobyManifest.metadata.author);
+  });
 
+  it('extracts links', async () => {
+    const manifest = await getManifest();
     // links
     // we will need to add a canonical self link eventually
-    expect(manifest.links).toEqual([]);
+    throw new Error();
+  });
 
+  it('extracts reading order', async () => {
+    const manifest = await getManifest();
     // reading order
     expect(manifest.readingOrder).toEqual(explodedMobyManifest.readingOrder);
+  });
 
+  it('extracts resources', async () => {
+    const manifest = await getManifest();
     // resources
     expect(manifest.resources).toEqual(explodedMobyManifest.resources);
+  });
 
-    // toc
+  it('extracts toc', async () => {
+    const manifest = await getManifest();
+    throw new Error();
   });
 });
