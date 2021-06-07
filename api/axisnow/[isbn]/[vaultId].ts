0ed26eec371a9c31939b2914094936152ecf04f4
kristojorg <kristo.jorgenson@gmail.com>
2021-06-07 12:08:21 -0400

    axisnow decryption proper

--- api/axisnow/[isbn]/[vaultId].ts
+++ api/axisnow/[isbn]/[vaultId].ts
@@ -7,6 +7,7 @@ import RemoteFetcher from '../../../src/RemoteFetcher';
 
 /**
  * This is a handler for Open eBooks Axisnow encrypted EPUBS.
+ * http://localhost:3000/api/axisnow/9781467784870/6734F7F5-C48F-4A38-9AE5-9DF4ADCFBF0A
  */
 export default async function epubToWebpub(
   req: VercelRequest,
@@ -14,23 +15,35 @@ export default async function epubToWebpub(
 ) {
   const book_vault_uuid = validateParam('vaultId', req.query);
   const isbn = validateParam('isbn', req.query);
+
+  if (!Decryptor) {
+    return res.status(500).json({
+      title: 'Missing AxisNow Decryptor',
+      detail:
+        '@nypl-simplified-packages/axisnow-access-control-web package was not included in build.',
+      status: 500,
+    });
+  }
+
   try {
-    const decryptor = await Decryptor?.createDecryptor({
+    const decryptor = await Decryptor.createDecryptor({
       book_vault_uuid,
       isbn,
     });
     const containerXmlHref = decryptor?.containerUrl;
     const fetcher = new RemoteFetcher(containerXmlHref, decryptor);
     const epub = await Epub.build(containerXmlHref, fetcher, decryptor);
-    const manifest = epub.webpubManifest;
+    const manifest = await epub.webpubManifest;
     res.status(200).json(manifest);
   } catch (e: unknown) {
     if (e instanceof Error) {
+      console.error(e);
       res.status(500).json({
         title: 'Epub Conversion Failure',
         detail: e.message,
         status: 500,
       });
+      return;
     }
     res.status(500).json({
       title: 'Epub Conversion Failure',
--- samples/axisnow/encrypted/README.txt
+++
@@ -1,2 +0,0 @@
-This is a sample AxisNow encrypted ePub. The details are:
-{"book_vault_uuid": "F58373FB-6574-45E6-B50E-6D73523AFD01", "isbn": "9781467784870"}
