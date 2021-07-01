b9926bbe0f3020d3209a3a8785028c755eb46235
Michael Benowitz <michaelbenowitz@nypl.org>
2021-07-01 14:01:10 -0400

    SFR-1064 First cut at lambda handler

---
+++ api/lambda.ts
@@ -0,0 +1,36 @@
+import { Context, APIGatewayProxyEvent } from 'aws-lambda';
+import Epub from '../src/Epub';
+import RemoteFetcher from '../src/RemoteFetcher';
+import { validateParam } from '../src/utils';
+
+exports.handler = async (event: APIGatewayProxyEvent, context: Context) => {
+  const containerXmlHref = validateParam('containerXml', event.pathParameters)
+
+  try {
+    const fetcher = new  RemoteFetcher(containerXmlHref);
+    const epub = await Epub.build(containerXmlHref, fetcher);
+    const manifest = await epub.webpubManifest;
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Cache-Control': 'maxage=0, s-maxage=1, stale-while-revalidate'
+      },
+      body: JSON.stringify(manifest)
+    }
+  } catch (e: unknown) {
+    let detail = 'UnknownError';
+ 
+    if (e instanceof Error) {
+      detail = e.message
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        title: 'Epub Conversion Failure',
+	      detail
+      })
+    }
+  }
+}
