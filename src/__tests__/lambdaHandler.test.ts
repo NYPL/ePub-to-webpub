bf510248d574694096b7fd936f0e48f5da4305b7
Michael Benowitz <michaelbenowitz@nypl.org>
2021-07-08 12:43:17 -0400

    SFR-1064 Linting new changes

--- src/__tests__/lambdaHandler.test.ts
+++ src/__tests__/lambdaHandler.test.ts
@@ -6,35 +6,37 @@ const epubHref = `${baseUrl}/samples/moby-epub3-exploded/META-INF/container.xml`
 describe('Lambda Handler', () => {
   it('should return JSON manifest on success', async () => {
     const testEvent = {
-			pathParameters: {
-				containerXml: epubHref
-			}
-    }
+      pathParameters: {
+        containerXml: epubHref,
+      },
+    };
 
-		const testResp = await handler(testEvent, {});
+    const testResp = await handler(testEvent, {});
 
-		expect(testResp.statusCode).toEqual(200);
-		expect(testResp.headers['Cache-Control']).toEqual('maxage=0, s-maxage=1, stale-while-revalidate');
+    expect(testResp.statusCode).toEqual(200);
+    expect(testResp.headers['Cache-Control']).toEqual(
+      'maxage=0, s-maxage=1, stale-while-revalidate'
+    );
 
-		const testBody = JSON.parse(testResp.body);
+    const testBody = JSON.parse(testResp.body);
 
-		expect(testBody.metadata.title).toEqual('Moby-Dick');
-  })
+    expect(testBody.metadata.title).toEqual('Moby-Dick');
+  });
 
-	it('should return 500 status on error', async () => {
-		const testEvent = {
-			pathParameters: {
-				containerXml: 'invalidURI'
-			}
-		}
+  it('should return 500 status on error', async () => {
+    const testEvent = {
+      pathParameters: {
+        containerXml: 'invalidURI',
+      },
+    };
 
-		const testResp = await handler(testEvent, {});
+    const testResp = await handler(testEvent, {});
 
-		expect(testResp.statusCode).toEqual(500);
-		expect(testResp.headers).toEqual(undefined);
+    expect(testResp.statusCode).toEqual(500);
+    expect(testResp.headers).toEqual(undefined);
 
-		const testBody = JSON.parse(testResp.body);
+    const testBody = JSON.parse(testResp.body);
 
-		expect(testBody.title).toEqual('Epub Conversion Failure');
-	})
-})
+    expect(testBody.title).toEqual('Epub Conversion Failure');
+  });
+});
--- src/container.ts
+++ src/container.ts
@@ -3,51 +3,47 @@ import Epub from './Epub';
 import RemoteFetcher from './RemoteFetcher';
 
 const handler = async (req: IncomingMessage, resp: ServerResponse) => {
-	const path = req.url ? req.url : '/';
+  const path = req.url ? req.url : '/';
 
-	const pathComponents = path.split('/').slice(1);
-	console.log(pathComponents)
-	if (pathComponents[0] === '') {
-		resp.writeHead(200, { 'Content-Type': 'application/json' });
-		resp.end(JSON.stringify({ service: 'epub-to-webpub' }), 'utf-8');
-	} else if (pathComponents[0] === 'api') {
-		const containerXmlHref = decodeURIComponent(pathComponents[1]);
+  const pathComponents = path.split('/').slice(1);
 
-		try {
-			const fetcher = new RemoteFetcher(containerXmlHref);
-			const epub = await Epub.build(containerXmlHref, fetcher);
-			const manifest = await epub.webpubManifest;
+  if (pathComponents[0] === '') {
+    resp.writeHead(200, { 'Content-Type': 'application/json' });
+    resp.end(JSON.stringify({ service: 'epub-to-webpub' }), 'utf-8');
+  } else if (pathComponents[0] === 'api') {
+    const containerXmlHref = decodeURIComponent(pathComponents[1]);
 
-			resp.writeHead(200, {
-				'Cache-Control': 'maxage=0, s-maxage=1, stale-while-revalidate',
-				'Content-Type': 'application/json',
-			});
-			resp.end(JSON.stringify(manifest), 'utf-8');
-		} catch (e: unknown) {
-			let detail = 'UnknownError';
+    try {
+      const fetcher = new RemoteFetcher(containerXmlHref);
+      const epub = await Epub.build(containerXmlHref, fetcher);
+      const manifest = await epub.webpubManifest;
 
-			if (e instanceof Error) {
-				detail = e.message;
-			}
+      resp.writeHead(200, {
+        'Cache-Control': 'maxage=0, s-maxage=1, stale-while-revalidate',
+        'Content-Type': 'application/json',
+      });
+      resp.end(JSON.stringify(manifest), 'utf-8');
+    } catch (e: unknown) {
+      let detail = 'UnknownError';
 
-			resp.writeHead(500, { 'Content-Type': 'application/json' });
-			resp.end(
-				JSON.stringify({ title: 'Epub Conversion Failure', detail }),
-				'utf-8'
-			);
-		}
-	} else {
-		resp.writeHead(404, { 'Content-Type': 'application/json' });
-		resp.end(
-			JSON.stringify({ title: 'Unknown path', detail: path }),
-			'utf-8'
-		);
-	}	
-}
+      if (e instanceof Error) {
+        detail = e.message;
+      }
+
+      resp.writeHead(500, { 'Content-Type': 'application/json' });
+      resp.end(
+        JSON.stringify({ title: 'Epub Conversion Failure', detail }),
+        'utf-8'
+      );
+    }
+  } else {
+    resp.writeHead(404, { 'Content-Type': 'application/json' });
+    resp.end(JSON.stringify({ title: 'Unknown path', detail: path }), 'utf-8');
+  }
+};
 
 createServer(async (req, resp) => {
-	await handler(req, resp);
-})
-.listen(5000);
+  await handler(req, resp);
+}).listen(5000);
 
-module.exports = { handler }
+module.exports = { handler };
